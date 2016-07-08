51dacc30f8fe44e858d6d20b946dc69f19f9823a
Parameswara Rao Mallela <mallelapr@gmail.com>
2016-07-08 19:27:24 -0400

    Implementing Suggestions
    Changed Hardcoded Values
    Implemented OnclickListener
    Toast work around
    Layout Header

--- app/src/main/java/com/param/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/param/android/myappportfolio/MainActivity.java
@@ -4,55 +4,82 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static Toast toast;
+    private Button btnPopluarMovies, btnStockHawk, btnBuildItBigger, btnMakeAppMaterial, btnGoUbiquitous, btnCapstone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButtonInit();
     }
 
-    public void popularMovie(View view) {
-        Button button = (Button) findViewById(R.id.btn_popular_movies);
-        displayToast(button);
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.btn_popular_movies:
+                displayToast(btnPopluarMovies);
+                break;
+
+            case R.id.btn_stock_hawk:
+                displayToast(btnStockHawk);
+                break;
+
+            case R.id.btn_buildit_bigger:
+                displayToast(btnBuildItBigger);
+                break;
+
+            case R.id.btn_make_app_material:
+                displayToast(btnMakeAppMaterial);
+                break;
+
+            case R.id.btn_go_ubiquitous:
+                displayToast(btnGoUbiquitous);
+                break;
+
+            case R.id.btn_capstone:
+                displayToast(btnCapstone);
+
+        }
+
     }
 
-    public void stockHawk(View view) {
-        Button button = (Button) findViewById(R.id.btn_stock_hawk);
-        displayToast(button);
-    }
+    private void ButtonInit() {
+        btnPopluarMovies = (Button) findViewById(R.id.btn_popular_movies);
+        btnStockHawk = (Button) findViewById(R.id.btn_stock_hawk);
+        btnBuildItBigger = (Button) findViewById(R.id.btn_buildit_bigger);
+        btnMakeAppMaterial = (Button) findViewById(R.id.btn_make_app_material);
+        btnGoUbiquitous = (Button) findViewById(R.id.btn_go_ubiquitous);
+        btnCapstone = (Button) findViewById(R.id.btn_capstone);
 
-    public void buildItBigger(View view) {
-        Button button = (Button) findViewById(R.id.btn_buildit_bigger);
-        displayToast(button);
-    }
-
-    public void makeYourAppMaterial(View view) {
-        Button button = (Button) findViewById(R.id.btn_make_app_material);
-        displayToast(button);
-    }
-
-    public void goUbiquitous(View view) {
-        Button button = (Button) findViewById(R.id.btn_go_ubiquitous);
-        displayToast(button);
-    }
-
-    public void capstone(View view) {
-        Button button = (Button) findViewById(R.id.btn_capstone);
-        displayToast(button);
+        btnCapstone.setOnClickListener(this);
+        btnGoUbiquitous.setOnClickListener(this);
+        btnMakeAppMaterial.setOnClickListener(this);
+        btnBuildItBigger.setOnClickListener(this);
+        btnPopluarMovies.setOnClickListener(this);
+        btnStockHawk.setOnClickListener(this);
     }
 
     public void displayToast(Button button) {
-        Context context = getApplicationContext();
-        String text = "This button will launch my ";
-        text += button.getText();
-        text += " app!";
-        int duration = Toast.LENGTH_SHORT;
-        Toast.makeText(context, text, duration).show();
+        showToast(this,
+                getString(R.string.toast_msg_1) +" "+
+                        button.getText() +" "+
+                        getString(R.string.toast_msg_2)
+        );
+    }
+
+    private void showToast(Context ctx, String message) {
+        if (toast != null) {
+            toast.cancel();
+        }
+        toast = Toast.makeText(ctx, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
